3ec572d7ead58695d77dbbf3eaef88e190409600
igorbohorquez <igorbohorquez3@gmail.com>
2022-10-06 17:27:26 -0300

    First commit

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed = 10f;
+    Rigidbody2D myRigidBody;
+    PlayerMovement player;
+    float xSpeed;
+
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovement>();
+        xSpeed = player.transform.localScale.x * projectileSpeed;
+    }
+
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(xSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            Destroy (other.gameObject);
+        }
+        Destroy (gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy (gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
+    [SerializeField] GameObject attack;
+    [SerializeField] Transform attackSpawnPoint;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+    float gravityScaleAtStart;
+
+    bool isAlive = true;
+
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
+    }
+
+
+    void Update()
+    {
+        if (!isAlive) { return; }
+        Run();
+        FlipSprite();
+        Die();
+    }
+
+    void OnFire(InputValue value)
+    {
+        if (!isAlive ) { return; }
+        Instantiate(attack, attackSpawnPoint.position, transform.rotation);
+    }
+
+    void OnMove(InputValue value)
+
+    {
+        if (!isAlive) { return; }
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!isAlive) { return; }
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+    void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            myRigidbody.velocity = deathKick;
+        }
+    }
+
+}
